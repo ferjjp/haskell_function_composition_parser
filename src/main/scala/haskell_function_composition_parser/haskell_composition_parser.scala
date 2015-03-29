8481966a4c014da012c958c1cfbdbac38afa6913
FernandoPetryszyn <fer_jjp@hotmail.com>
2015-03-29 17:42:02 -0300

    Parses function with arguments

--- src/main/scala/haskell_function_composition_parser/haskell_composition_parser.scala
+++ src/main/scala/haskell_function_composition_parser/haskell_composition_parser.scala
@@ -12,7 +12,7 @@ trait HaskellCompositionParser extends RegexParsers {
   
   lazy protected val composition = (function <~ ".") ~ function ^^ {case funcion ~ funcion1 => Composition(funcion,funcion1)}
   lazy protected val commentSkipper = "-{2,}(.*)$".r
-  lazy protected val function = identifier ~ repsep(argument," ") ^^ {case id ~ arguments => Function(id,arguments)}
+  lazy protected val function = identifier ~ repsep(argument,"") ^^ {case id ~ arguments => Function(id,arguments)}
   lazy protected val argument = identifier ^^ { r => Argument(r)}
   lazy protected val identifier = """[A-Za-z]+""".r 
   def apply(input: String) = parseAll(???, input) match {
--- src/test/scala/haskell_function_composition_parser/haskell_composition_parser_test.scala
+++ src/test/scala/haskell_function_composition_parser/haskell_composition_parser_test.scala
@@ -5,10 +5,17 @@ import org.scalatest.FreeSpec
 
 class haskell_composition_parser_test extends FreeSpec with ParserTest[HaskellCompositionParser] with HaskellCompositionParser {
   "should parse functions" - { 
+    
    "should parse a simple function" in {
      """functionA""" should beParsedTo(Function("functionA",Nil))(function)
    }
+   
+   "should parse a function with arguments" in {
+     """functionA bla ble bli""" should beParsedTo(Function("functionA",List(Argument("bla"),Argument("ble"),Argument("bli"))))(function)
+   }
+   
  }
+  
   "should parse compositions" - {
    "should parse a simple composition" in {
      """funcionA.funcionB""" should beParsedTo(Composition(Function("funcionA",Nil),Function("funcionB",Nil)))(composition)
